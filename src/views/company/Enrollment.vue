5afc70131bf6683bb3a86a98b1307a2692b0e1bb
noelou jan <noelounagac@gmail.com>
2019-05-09 17:24:16 +0800

    add web-input-container and change the naming convention of the button in enrollment

--- src/views/company/Enrollment.vue
+++ src/views/company/Enrollment.vue
@@ -40,83 +40,149 @@
           </div>
         </div>
         <!-------- WEB INPUT -------->
-        <div v-if="isState === 'web'" class="employee-details-wrapper">
-          <span class="employee-tier-title">
-            EMPLOYEE
-            <span>17</span> OF
-            <span>27</span>
-          </span>
-          <div class="employee-details-header">
-            <h1>Employee Details</h1>
-            <div class="add-dependent-btn">
-              <h4>Add a Dependent?</h4>
-              <button @click="addDependent">
-                <img :src="'../assets/img/icons/add-employee.svg'"> Add
+        <div class="web-input-wrapper">
+          <div v-if="isState === 'web'" class="employee-details-wrapper">
+            <span class="employee-tier-title">
+              EMPLOYEE
+              <span>17</span> OF
+              <span>27</span>
+            </span>
+            <div class="employee-details-header">
+              <h1>Employee Details</h1>
+              <div class="add-dependent-btn">
+                <h4>Add a Dependent?</h4>
+                <button @click="addDependent">
+                  <img :src="'../assets/img/icons/add-employee.svg'"> Add
+                </button>
+              </div>
+            </div>
+            <form>
+              <div class="employee-input-container">
+                <div class="employee-input-wrapper">
+                  <label for="fname">First / Given Name</label>
+                  <input type="text" name="fname">
+                </div>
+                <div class="employee-input-wrapper">
+                  <label for="fname">Last / Family Name</label>
+                  <input type="text" name="lname">
+                </div>
+              </div>
+              <div class="employee-input-container">
+                <div class="employee-input-wrapper nric">
+                  <label>
+                    <input type="radio" name="id_status">
+                    NRIC
+                  </label>
+                  <label>
+                    <input type="radio" name="id_status">
+                    FIN
+                  </label>
+                  <input type="text" name="nric-fin">
+                </div>
+                <div class="employee-input-wrapper dob">
+                  <label for="fname">Date of Birth</label>
+                  <input type="text" name="lname" placeholder="DD/MM/YYYY">
+                </div>
+              </div>
+              <div class="employee-input-container">
+                <div class="employee-input-wrapper">
+                  <label for="fname">Work Email</label>
+                  <input type="text" name="work-email">
+                </div>
+                <div class="employee-input-wrapper">
+                  <label for="fname">Mobile</label>
+                  <input type="text" name="mobile">
+                </div>
+              </div>
+              <div class="employee-input-container">
+                <div class="employee-input-wrapper">
+                  <label for="fname">Postal Code</label>
+                  <input type="text" name="postal-code">
+                </div>
+                <div class="employee-input-wrapper">
+                  <label for="fname">Start Date</label>
+                  <input type="text" name="start-date">
+                </div>
+              </div>
+              <div class="employee-input-container">
+                <div class="employee-input-wrapper">
+                  <label for="fname" class="medical-text">Medical Credits</label>
+                  <label class="subtext">*If there are no credits to allocate, please key in 0</label>
+                  <input type="number" min="0" value="0" name="medical-credits">
+                </div>
+                <div class="employee-input-wrapper">
+                  <label for="fname" class="wellness-text">Wellness Credits</label>
+                  <label class="subtext">*If there are no credits to allocate, please key in 0</label>
+                  <input type="number" min="0" value="0" name="wellness-credits">
+                </div>
+              </div>
+            </form>
+           
+          </div>
+          <!-- Add dependent section -->
+          <div class="dependent-details-wrapper" v-if="depdentState">
+            <span class="employee-tier-title">
+              DEPENDENT
+              <span>4</span> OF
+              <span>4</span>
+            </span>
+            <div class="employee-details-header">
+              <h1>Dependent details</h1>
+              <button v-if="false" class="btn-remove">
+                <img :src="'../assets/img/icons/dustbin.png'">Remove
               </button>
             </div>
+            <form class="form-input-container">
+              <div class="employee-input-container">
+                <div class="employee-input-wrapper">
+                  <label for="fname">First / Given Name</label>
+                  <input type="text" name="fname">
+                </div>
+                <div class="employee-input-wrapper">
+                  <label for="fname">Last / Family Name</label>
+                  <input type="text" name="lname">
+                </div>
+              </div>
+              <div class="employee-input-container">
+                <div class="employee-input-wrapper nric">
+                  <label>NRIC</label>
+                  <label>FIN</label>
+                  <input type="text" name="nric-fin">
+                </div>
+                <div class="employee-input-wrapper dob">
+                  <label for="fname">Date of Birth</label>
+                  <input type="text" name="lname" placeholder="DD/MM/YYYY">
+                </div>
+              </div>
+              <div class="employee-input-container">
+                <div class="employee-input-wrapper">
+                  <label for="fname">Relationship</label>
+                  <select>
+                    <option>Spouse</option>
+                    <option>Child</option>
+                    <option>Family</option>
+                  </select>
+                  <img :src="'../assets/img/icons/down-arrow.svg'">
+                </div>
+                <div class="employee-input-wrapper">
+                  <label for="fname">Start Date</label>
+                  <input type="text" name="stard-date">
+                </div>
+              </div>
+            </form>
+            <div class="summary-left-right-btn" v-if="false">
+              <img :src="'../assets/img/icons/left.png'">
+              <img :src="'../assets/img/icons/right.png'">
+            </div>
+            <div class="dependent-details-btn">
+              <button @click="addDependent" class="btn-cancel">CANCEL</button>
+              <div class="btn-right-container">
+                <button class="btn-add">ADD</button>
+                <button class="btn-save-continue">SAVE & CONTINUE</button>
+              </div>
+            </div>
           </div>
-          <form>
-            <div class="employee-input-container">
-              <div class="employee-input-wrapper">
-                <label for="fname">First / Given Name</label>
-                <input type="text" name="fname">
-              </div>
-              <div class="employee-input-wrapper">
-                <label for="fname">Last / Family Name</label>
-                <input type="text" name="lname">
-              </div>
-            </div>
-            <div class="employee-input-container">
-              <div class="employee-input-wrapper nric">
-                <label>
-                  <input type="radio" name="id_status">
-                  NRIC
-                </label>
-                <label>
-                  <input type="radio" name="id_status">
-                  FIN
-                </label>
-                <input type="text" name="nric-fin">
-              </div>
-              <div class="employee-input-wrapper dob">
-                <label for="fname">Date of Birth</label>
-                <input type="text" name="lname" placeholder="DD/MM/YYYY">
-              </div>
-            </div>
-            <div class="employee-input-container">
-              <div class="employee-input-wrapper">
-                <label for="fname">Work Email</label>
-                <input type="text" name="work-email">
-              </div>
-              <div class="employee-input-wrapper">
-                <label for="fname">Mobile</label>
-                <input type="text" name="mobile">
-              </div>
-            </div>
-            <div class="employee-input-container">
-              <div class="employee-input-wrapper">
-                <label for="fname">Postal Code</label>
-                <input type="text" name="postal-code">
-              </div>
-              <div class="employee-input-wrapper">
-                <label for="fname">Start Date</label>
-                <input type="text" name="start-date">
-              </div>
-            </div>
-            <div class="employee-input-container">
-              <div class="employee-input-wrapper">
-                <label for="fname" class="medical-text">Medical Credits</label>
-                <label class="subtext">*If there are no credits to allocate, please key in 0</label>
-                <input type="number" min="0" value="0" name="medical-credits">
-              </div>
-              <div class="employee-input-wrapper">
-                <label for="fname" class="wellness-text">Wellness Credits</label>
-                <label class="subtext">*If there are no credits to allocate, please key in 0</label>
-                <input type="number" min="0" value="0" name="wellness-credits">
-              </div>
-            </div>
-          </form>
-          <!-- side content summary -->
+           <!-- side content summary -->
           <div class="summary-right-container">
             <button id="summary-btn" class="summary-right-button" @click="toggleSummary">SUMMARY</button>
             <div id="summary-content" class="list-of-employee list-employee-active">
@@ -187,69 +253,6 @@
             </div>
           </div>
         </div>
-        <!-- Add dependent section -->
-        <div class="dependent-details-wrapper" v-if="depdentState">
-          <span class="employee-tier-title">
-            DEPENDENT
-            <span>4</span> OF
-            <span>4</span>
-          </span>
-          <div class="employee-details-header">
-            <h1>Dependent details</h1>
-            <button v-if="false" class="btn-remove">
-              <img :src="'../assets/img/icons/dustbin.png'">Remove
-            </button>
-          </div>
-          <form class="form-input-container">
-            <div class="employee-input-container">
-              <div class="employee-input-wrapper">
-                <label for="fname">First / Given Name</label>
-                <input type="text" name="fname">
-              </div>
-              <div class="employee-input-wrapper">
-                <label for="fname">Last / Family Name</label>
-                <input type="text" name="lname">
-              </div>
-            </div>
-            <div class="employee-input-container">
-              <div class="employee-input-wrapper nric">
-                <label>NRIC</label>
-                <label>FIN</label>
-                <input type="text" name="nric-fin">
-              </div>
-              <div class="employee-input-wrapper dob">
-                <label for="fname">Date of Birth</label>
-                <input type="text" name="lname" placeholder="DD/MM/YYYY">
-              </div>
-            </div>
-            <div class="employee-input-container">
-              <div class="employee-input-wrapper">
-                <label for="fname">Relationship</label>
-                <select>
-                  <option>Spouse</option>
-                  <option>Child</option>
-                  <option>Family</option>
-                </select>
-                <img :src="'../assets/img/icons/down-arrow.svg'">
-              </div>
-              <div class="employee-input-wrapper">
-                <label for="fname">Start Date</label>
-                <input type="text" name="stard-date">
-              </div>
-            </div>
-          </form>
-          <div class="summary-left-right-btn" v-if="false">
-            <img :src="'../assets/img/icons/left.png'">
-            <img :src="'../assets/img/icons/right.png'">
-          </div>
-          <div class="dependent-details-btn">
-            <button @click="addDependent" class="btn-cancel">CANCEL</button>
-            <div class="btn-right-container">
-              <button class="btn-add">ADD</button>
-              <button class="btn-save-continue">SAVE & CONTINUE</button>
-            </div>
-          </div>
-        </div>
       </div>
 
       <div class="prev-next-button-container">
@@ -262,9 +265,9 @@
           >Back</button>
 
           <div class="btn-enroll-container">
-            <button v-if="isState === 'false'" class="btn-next-employee">DELETE</button>
-            <button v-if="isState === 'false'" class="btn-next-employee">PREVIOUS EMPLOYEE</button>
-            <button v-if="isState === 'web'" class="btn-next-employee">NEXT EMPLOYEE</button>
+            <button v-if="isState === 'false'" class="btn-employee">DELETE</button>
+            <button v-if="isState === 'false'" class="btn-employee">PREVIOUS EMPLOYEE</button>
+            <button v-if="isState === 'web'" class="btn-employee">NEXT EMPLOYEE</button>
             <button class="next-btn" v-if="isState === 'enrollment'" v-on:click="next">Next</button>
             <button class="next-btn" v-if="isState === 'web' || isState === 'dependent'" v-on:click="employeeDetails">Next</button>
             <button class="next-btn" v-if="isState === 'excel'" v-on:click="excel">Next</button>
