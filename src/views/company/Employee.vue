9d60338c44397ec565843a7fb377ffd8dcd49bf7
noelou jan <noelounagac@gmail.com>
2019-05-27 14:48:41 +0800

    fix the css style of employee and add some contents for health spending account summary

--- src/views/company/Employee.vue
+++ src/views/company/Employee.vue
@@ -539,6 +539,73 @@
                       <span> <strong>Current</strong> - <span class="account-summary-date">19/03/2019</span></span>
                     </div>
                   </div>
+                  <div class="medical-wellness-wrapper">
+                    <div class="medical-container">
+                      <h4>Medical Spending Account</h4>
+                      <div class="spending-account-details">
+                        <div class="bills-spending-container">
+                          <strong>Initial Allocation</strong>
+                          <div>S$ <span>0.00</span></div>
+                        </div>
+                        <div class="bills-spending-container pro-rated">
+                          <strong>Pro-rated Allocation</strong>
+                          <div>S$ <span>0.00</span></div>
+                        </div>
+                        <div class="bills-spending-container current-usage">
+                          <strong>Current Usage</strong>
+                          <div>S$ <span>0.00</span></div>
+                        </div>
+                        <div class="current-usage-details">
+                          <div class="bills-spending-container">
+                            <span>Spent</span>
+                            <div>S$ <span>0.00</span></div>
+                          </div>
+                          <div class="bills-spending-container">
+                            Pending claim
+                            <div>S$ <span>0.00</span></div>
+                          </div>
+                        </div>
+                        <div class="bills-spending-container balance">
+                          <strong>Balance</strong>
+                          <div>S$ <span>0.00</span></div>
+                        </div>
+                      </div>
+                      <div class="spending-account-status on-track">On Track</div>
+                    </div>
+                    <div class="separator"></div>
+                    <div class="wellness-container">
+                      <h4 class="font-helvetica-medium">Wellness Spending Account</h4>
+                      <div class="spending-account-details">
+                        <div class="bills-spending-container">
+                          <strong>Initial Allocation</strong>
+                          <div>S$ <span>0.00</span></div>
+                        </div>
+                        <div class="bills-spending-container pro-rated">
+                          <strong>Pro-rated Allocation</strong>
+                          <div>S$ <span>0.00</span></div>
+                        </div>
+                        <div class="bills-spending-container current-usage">
+                          <strong>Current Usage</strong>
+                          <div>S$ <span>0.00</span></div>
+                        </div>
+                        <div class="current-usage-details">
+                          <div class="bills-spending-container">
+                            <span>Spent</span>
+                            <div>S$ <span >0.00</span></div>
+                          </div>
+                          <div class="bills-spending-container">
+                            <span>Pending claim</span>
+                            <div>S$ <span>0.00</span></div>
+                          </div>
+                        </div>
+                        <div class="bills-spending-container balance">
+                          <strong>Balance</strong>
+                          <div>S$ <span>0.00</span></div>
+                        </div>
+                      </div>
+                      <div class="spending-account-status exceed">Exceed</div>
+                    </div>
+                  </div>
                 </div>
               </FadeTransition>
             </div>
