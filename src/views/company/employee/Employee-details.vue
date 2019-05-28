636b2df1d605f665431082171e6c17ab4b900979
noelou jan <noelounagac@gmail.com>
2019-05-28 14:29:59 +0800

    add some contents for remove employee

--- src/views/company/employee/Employee-details.vue
+++ src/views/company/employee/Employee-details.vue
@@ -477,7 +477,7 @@
             </div>
           </div>
           <div class="employee-input-container">
-            <div class="employee-input-wrapper dob">
+            <div class="employee-input-wrapper remove-dob">
               <label for="">Date of Birth</label>
               <v-date-picker
                   :max-date='new Date()'
@@ -488,22 +488,112 @@
           </div>
         </form>
       </div>
-      <div v-if="true" class="">
+
+      <div v-if="false" class="employee-outcome-wrapper">
         <h1>How would you like to proceed?</h1>
         <div class="employee-outcome-container">
-          <span class="outcome-title">Please select one of the outcome:</span>
+          <div class="outcome-title">Please select one of the outcome:</div>
           <label class="review-container">
-            <input type="checkbox" value="1">
+            <input type="checkbox">
             <span class="review-prepare-template-text">To replace the leaving employee, I would like to pre-enroll the new joiner. </span>
             <span class="review-checkmark"></span>
           </label>
-            <label class="review-container">
-            <input type="checkbox" value="1">
+          <label class="review-container">
+            <input type="checkbox">
             <span class="review-prepare-template-text">I'm not ready to pre-enroll the new joiner, please hold the seat for future hire. </span>
-            <span class="review-prepare-template-text">*Note: Once this employee is removed, the occupied seat will move to a vacant seat.</span>
+            <span class="review-prepare-template-subtext">*Note: Once this employee is removed, the occupied seat will move to a vacant seat.</span>
+            <span class="review-checkmark"></span>
+          </label>
+          <label class="review-container">
+            <input type="checkbox">
+            <span class="review-prepare-template-text">Please remove the seat completely, and proceed for refund.</span>
             <span class="review-checkmark"></span>
           </label>
         </div>
+      </div>
+
+      <!-- Replacement Employee Details-->
+      <div v-if="true" class="replacement-employee-wrapper">
+        <div class="employee-details-wrapper">
+          <div class="employee-tier-title">Replacement</div>
+          <div class="employee-details-header">
+            <h1>Employee Details</h1>
+          </div>
+          <form>
+            <div class="employee-input-container">
+              <div class="employee-input-wrapper">
+                <label for="fname">First / Given Name</label>
+                <input type="text" name="fname"></div>
+              <div class="employee-input-wrapper">
+                <label for="fname">Last / Family Name</label>
+                <input type="text" name="lname"></div>
+            </div>
+            <div class="employee-input-container">
+              <div class="employee-input-wrapper nric">
+                <label>
+                  <input type="radio" name="id_status" value="nric">
+                NRIC
+                </label>
+                <label>
+                  <input type="radio" name="id_status" value="fin">
+                FIN
+                </label>
+                <input type="text" name="nric-fin">
+              </div>
+              <div class="employee-input-wrapper dob">
+                <label>Date of Birth</label>
+                <v-date-picker
+                    popoverDirection="top"
+                    :max-date='new Date()'
+                    :input-props='{class: "vDatepicker", placeholder: "MM/DD/YYYY", readonly: true, }'
+                >
+                </v-date-picker>
+              </div>
+            </div>
+            <div class="employee-input-container">
+              <div class="employee-input-wrapper">
+                <label for="fname">Work Email</label>
+                <input type="email" name="work-email">
+              </div>
+              <div class="employee-input-wrapper">
+                <label for="fname">Mobile</label>
+                <input type="number" name="mobile">
+              </div>
+            </div>
+            <div class="employee-input-container">
+              <div class="employee-input-wrapper">
+                <label for="fname">Postal Code</label>
+                <input type="text" name="postal-code">
+              </div>
+              <div class="employee-input-wrapper">
+                <label>Start Date</label>
+                <v-date-picker
+                    popoverDirection="top"
+                    :date='new Date()'
+                    :input-props='{class: "vDatepicker", placeholder: "MM/DD/YYYY", readonly: true, }'
+                >
+                </v-date-picker>
+              </div>
+            </div>
+            <div class="employee-input-container">
+              <div class="employee-input-wrapper">
+                <label for="fname" class="medical-text">Medical Credits</label>
+                <label class="subtext">*If there are no credits to allocate, please key in 0</label>
+                <input type="number" min="0" value="0" name="medical-credits">
+              </div>
+              <div class="employee-input-wrapper">
+                <label for="fname" class="wellness-text">Wellness Credits</label>
+                <label class="subtext">*If there are no credits to allocate, please key in 0</label>
+                <input type="number" min="0" value="0" name="wellness-credits">
+              </div>
+            </div>
+          </form>
+          </div>
+      </div>
+
+      <!-- Health Spending Account Summary -->
+      <div v-if="false" class="">
+        
       </div>
 
       <div class="prev-next-button-container">
