74fb0e8a6c05ea4b9d51731e112b4c9bb8534ea4
jkpz10 <zayas.j10@gmail.com>
2019-05-29 15:37:27 +0800

    fix delete

--- src/components/company/employee/employee-details.vue
+++ src/components/company/employee/employee-details.vue
@@ -1,91 +1,89 @@
 <script>
-
 import Modal from "../../../views/company/modal/Modal.vue";
 import FadeTransition from "../../../assets/transitions/FadeTransition";
 import ModalTransition from "../../../assets/transitions/ModalTransition";
 
 // Methods here
 let employeeDetails = {
-	components: {
-		Modal,
+  components: {
+    Modal,
     FadeTransition,
     ModalTransition
   },
   data() {
     return {
-    	toggle_overiew_type: 1,
+      toggle_overiew_type: 1,
       isMedicalUsageShow: false,
       isWellnessUsageShow: false,
-			healthSpendingAccountTabIsShow: false,
-			modalEditEmployee: false,
+      healthSpendingAccountTabIsShow: false,
+      modalEditEmployee: false,
       modalEdit: {
-				employee: false,
-				dependent: false,
-			},
+        employee: false,
+        dependent: false
+      },
       showAddDependents: false,
-      showRemoveEmployee: false,
+      showRemoveEmployee: false
     };
   },
   methods: {
-  	editDetailsData( data ) {
+    editDetailsData(data) {
       this.modalEditEmployee = data.modalEditEmployee;
-      console.log(data.modalEditEmployee);
     },
-  	toggleTabOverview( opt ) {
-			this.toggle_overiew_type = opt;
-			this.healthSpendingAccountTabIsShow = false;
-		},
-  	toggleMedicalUsage() {
-			if(this.isMedicalUsageShow == false ) {
-				this.isMedicalUsageShow = true;
-			}else {
-				this.isMedicalUsageShow = false;
-			}
-		},
-		toggleWellnessUsage() {
-			if(this.isWellnessUsageShow == false ) {
-				this.isWellnessUsageShow = true;
-			}else {
-				this.isWellnessUsageShow = false;
-			}
-		},
-		toggleAddDependents( data ) {
-			let x = data;
-			if( x === 'add' ) {
-				console.log('ni gana');
-				this.showAddDependents = true;
-			} else if( x === 'cancel' ) {
-				this.showAddDependents = false;
-			}
-		},
-		removeEmployee( data ) {
-			let x = data;
-			if( x === 'showRemove' ) {
-				this.showRemoveEmployee = true;
-			} else if( x === 'cancel' ) {
-				this.showRemoveEmployee = false;
-			}
-		},
-		// openUpdateEmployeeModal( data ) {
-		// 	let x = data;
-		// 	if( x === 'edit' ) {
-		// 		this.modalEdit.employee = !this.modalEdit.employee;
-		// 	} else if( x === 'close' ) {
-		// 		this.modalEdit.employee = !this.modalEdit.employee;
-		// 	}
-		// },
-		// openUpdateDependentModal( data ) {
-		// 	let x = data;
-		// 	if( x === 'edit' ) {
-		// 		this.modalEdit.dependent = !this.modalEdit.dependent;
-		// 	} else if( x === 'close' ) {
-		// 		this.modalEdit.dependent = !this.modalEdit.dependent;
-		// 	}
-		// },
-		reset() {
-			this.$swal({
+    toggleTabOverview(opt) {
+      this.toggle_overiew_type = opt;
+      this.healthSpendingAccountTabIsShow = false;
+    },
+    toggleMedicalUsage() {
+      if (this.isMedicalUsageShow == false) {
+        this.isMedicalUsageShow = true;
+      } else {
+        this.isMedicalUsageShow = false;
+      }
+    },
+    toggleWellnessUsage() {
+      if (this.isWellnessUsageShow == false) {
+        this.isWellnessUsageShow = true;
+      } else {
+        this.isWellnessUsageShow = false;
+      }
+    },
+    toggleAddDependents(data) {
+      let x = data;
+      if (x === "add") {
+        this.showAddDependents = true;
+      } else if (x === "cancel") {
+        this.showAddDependents = false;
+      }
+    },
+    removeEmployee(data) {
+      let x = data;
+      if (x === "showRemove") {
+        this.showRemoveEmployee = true;
+      } else if (x === "cancel") {
+        this.showRemoveEmployee = false;
+      }
+    },
+    // openUpdateEmployeeModal( data ) {
+    // 	let x = data;
+    // 	if( x === 'edit' ) {
+    // 		this.modalEdit.employee = !this.modalEdit.employee;
+    // 	} else if( x === 'close' ) {
+    // 		this.modalEdit.employee = !this.modalEdit.employee;
+    // 	}
+    // },
+    // openUpdateDependentModal( data ) {
+    // 	let x = data;
+    // 	if( x === 'edit' ) {
+    // 		this.modalEdit.dependent = !this.modalEdit.dependent;
+    // 	} else if( x === 'close' ) {
+    // 		this.modalEdit.dependent = !this.modalEdit.dependent;
+    // 	}
+    // },
+    reset() {
+      this.$swal({
         title: "Confirm",
-        text: "Are you sure you want to resend and reset the password for this account?",
+        text:
+          "Are you sure you want to resend and reset the password for this account?",
         type: "warning",
         showCancelButton: true,
         confirmButtonText: "Yes",
@@ -97,26 +95,24 @@ let employeeDetails = {
           this.isState = "web";
         }
       });
-		},
-		viewEmployeeSpendingSummary() {
-
-			if (this.healthSpendingAccountTabIsShow == false ) {
-				this.toggle_overiew_type = 4;
-				this.healthSpendingAccountTabIsShow = true;
-			}else {
-				this.toggle_overiew_type = 1;
-				this.healthSpendingAccountTabIsShow = false;
-			}
-		}
+    },
+    viewEmployeeSpendingSummary() {
+      if (this.healthSpendingAccountTabIsShow == false) {
+        this.toggle_overiew_type = 4;
+        this.healthSpendingAccountTabIsShow = true;
+      } else {
+        this.toggle_overiew_type = 1;
+        this.healthSpendingAccountTabIsShow = false;
+      }
+    }
   },
-  created () {
-  }
+  created() {}
 };
 
-export default employeeDetails
+export default employeeDetails;
 </script>
 
 
 <style lang="scss" scoped>
-	@import './src/assets/css/company/employee.scss'
+@import "./src/assets/css/company/employee.scss";
 </style>
