5923b766cbeb95934bd3acfeb235ef23ee05a10d
jkpz10 <zayas.j10@gmail.com>
2019-06-03 11:25:42 +0800

    separate script methods in view foldert to components folder

--- src/views/company/enrollment/Excel.vue
+++ src/views/company/enrollment/Excel.vue
@@ -1,77 +1,3 @@
-<script>
-  export default {
-    components: {},
-    data() {
-      return {
-        isState: 'excel',
-        stepperState: 'download',
-        stepStatus : 1,
-        empType: "empOnly",
-        checkedlistEmpOnly: [],
-        checkedlistEmpDependents: [],
-      };
-    },
-    methods: {
-      empBtnType(data) { //used in excell
-      this.empType = data;
-      },
-      excel(data) { //used in excel
-        if (data == 'empOnly' || data == 'empDependents') {
-          this.checkedlistEmpOnly = [];
-          this.checkedlistEmpDependents = [];
-          if (this.empType === "empOnly") {
-            this.stepperState = "empOnly";
-            ++this.stepStatus;
-            this.$emit('excelData', {
-              stepStatus: this.stepStatus
-            });
-          } else if (this.empType === "empDependents") {
-            this.stepperState = "empDependents";
-            ++this.stepStatus;
-            this.$emit('excelData', {
-              stepStatus: this.stepStatus
-            });
-          }
-        } else if (data == 'upload') {
-          if (this.checkedlistEmpOnly.length == 5) {
-            this.stepperState = "upload";
-            ++this.stepStatus;
-            this.$emit('excelData', {
-              stepStatus: this.stepStatus
-            });
-          } else if (this.checkedlistEmpDependents.length == 6) {
-            this.stepperState = "upload";
-            ++this.stepStatus;
-            this.$emit('excelData', {
-              stepStatus: this.stepStatus
-            });
-          }
-        } else if (data == 'enrollsum') {
-          this.stepperState = null;
-          this.$router.push('summary');
-          ++this.stepStatus;
-          this.$emit('excelData', {
-            stepStatus: this.stepStatus
-          });
-        }
-      },
-      back(data) {
-        if (data == "download") {
-          this.stepperState = "download";
-          this.stepStatus = 1;
-          this.$emit('excelData', {
-              stepStatus: this.stepStatus
-            });
-        }
-      
-      },
-    },
-    created() {
-    }
-
-  }
-</script>
-
 <template>
   <transition name="fade">
     <div class="enrollment-wrapper">
@@ -248,6 +174,7 @@
   </transition>
 </template>
 
-<style lang="scss">
-  @import "./src/assets/css/company/enrollment.scss";
-</style>
+<script>
+import excel from "@/components/company/enrollment/excel";
+export default excel
+</script>
