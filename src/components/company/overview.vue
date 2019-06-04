9beeaf6a3da05d9770515f7d2c7e27b4697d37d9
jkpz10 <zayas.j10@gmail.com>
2019-06-04 14:14:01 +0800

    edit toggle methods in dashboard

--- src/components/company/overview.vue
+++ src/components/company/overview.vue
@@ -3,13 +3,18 @@
 let overview = {
   data() {
     return {
-      spendingType: 'medical' //default is medical
+      spendingType:{
+        value: 0,
+        text: ''
+      } //default is medical
     };
   },
   methods: {
-    spendType(data){
+    spendType(value, text){
       //spending either medical or wellness
-      this.spendingType = data;
+      this.spendingType.value = value;
+      this.spendingType.text = text;
+      console.log(this.spendingType);
     },
     //emit/transfer data data to another component
     toEnrollment(data) {
--- src/views/company/Overview.vue
+++ src/views/company/Overview.vue
@@ -146,8 +146,8 @@
             </div>
             <div class="home-spending-type-container">
               <p>Spending Account</p>
-              <button @click="spendType('medical')" v-bind:class="{'active': spendingType === 'medical'}" class="btn btn-medical">Medical</button>
-              <button @click="spendType('wellness')" v-bind:class="{'active': spendingType === 'wellness'}" class="btn btn-wellness">Wellness</button>
+              <button @click="spendType(0, 'medical')" v-bind:class="{'active': spendingType.value === 0}" class="btn btn-medical">Medical</button>
+              <button @click="spendType(1, 'wellness')" v-bind:class="{'active': spendingType.value === 1}" class="btn btn-wellness">Wellness</button>
             </div>
             <div class="credit-box">
               <div class="credit-allocated-container">
