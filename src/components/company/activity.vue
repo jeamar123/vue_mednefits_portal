dbba4e65ba8b7f8ff3e16bf62e8cc2b216e3ba84
jkpz10 <zayas.j10@gmail.com>
2019-06-06 11:19:58 +0800

    add toggle on spending type and in-out-network

--- src/components/company/activity.vue
+++ src/components/company/activity.vue
@@ -1,17 +1,37 @@
 <script>
-// methods here 
+// methods here
 let activity = {
   data() {
     return {
-      timeFrame: {},
+      spendingType: {
+        value: 0,
+        text: ""
+      },
+      networkType: {
+        value: 0,
+        text: ""
+      },
+      timeFrame: {}
     };
   },
+  methods: {
+    spendType(value, text) {
+      //spending either medical or wellness
+      this.spendingType.value = value;
+      this.spendingType.text = text;
+    },
+    netType(value, text) {
+      //spending either medical or wellness
+      this.networkType.value = value;
+      this.networkType.text = text;
+    }
+  }
 };
 
-export default activity
+export default activity;
 </script>
 
 
 <style lang="scss">
-  @import './src/assets/css/company/activity.scss'
+@import "./src/assets/css/company/activity.scss";
 </style>
