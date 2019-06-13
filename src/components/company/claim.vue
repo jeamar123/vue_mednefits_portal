4ca092aa54e4d5172581bf504974f3b85e7bdb1a
noelou jan <noelounagac@gmail.com>
2019-06-13 16:44:40 +0800

    add contents for claim

--- src/components/company/claim.vue
+++ src/components/company/claim.vue
@@ -3,11 +3,27 @@
 let claim = {
   data() {
     return {
-      sampleData: true
+    	eclaimSpendingType: {
+    		value: 0,
+        text: ""
+    	},
+      timeFrame: {},
     };
+  },
+  methods: {
+  	spendType(value, text) {
+      //spending either medical or wellness
+      this.eclaimSpendingType.value = value;
+      this.eclaimSpendingType.text = text;
+    },
   }
 };
 
 export default claim;
 </script>
+
+<style lang="scss" scoped>
+@import "./src/assets/css/company/claim.scss";
+</style>
+
 
--- src/views/company/Claim.vue
+++ src/views/company/Claim.vue
@@ -1,6 +1,209 @@
 <template>
-  <div>
-    <h1>Claim</h1>
+  <div class="claim-wrapper">
+  	<div class="container">
+  		<div class="claim-container">
+    		<span class="timeframe-title">Select a timeframe (Claim Date)</span>
+
+    		<div class="activity-header-calendar">
+					<div class="date-selection-container">
+
+						<div class="custom-date-selector">
+							<i class="fa fa-calendar"></i>
+							<v-date-picker :max-date='new Date()' v-model="timeFrame.start"
+                :input-props='{class: "activity-custom-input", placeholder: "MM/DD/YYYY", readonly: true}'
+                popover-visibility='focus'>
+              </v-date-picker>
+							<i class="fa fa-caret-down"></i>
+						</div>
+
+						<span><i class="fa fa-arrow-right"></i></span>
+						
+						<div class="custom-date-selector">	
+							<i class="fa fa-calendar"></i>
+							<v-date-picker :max-date='new Date()' v-model="timeFrame.end"
+                :input-props='{class: "activity-custom-input", placeholder: "MM/DD/YYYY", readonly: true}'
+                popover-visibility='focus'>
+              </v-date-picker>
+							<i class="fa fa-caret-down"></i>	
+						</div>		
+
+						<button class="btn-apply">
+							Apply
+						</button>
+						
+					</div>
+				</div>
+
+				<div class="team-benefits-wrapper">
+					<div class="team-benefits-header">
+						<h4>Team E-Claim</h4>
+						<div class="spending-type-container">
+							<button @click="spendType(0, 'medical')" v-bind:class="{'active': eclaimSpendingType.value === 0}" class="btn-spending-type btn-medical">Medical</button>
+							<button @click="spendType(1, 'wellness')" v-bind:class="{'active': eclaimSpendingType.value === 1}" class="btn-spending-type btn-wellness">Wellness</button>
+						</div>
+					</div>
+					<div class="cost-wrapper">
+						<div class="benefit-box">
+							<h5>S$ <span>0.00</span></h5>
+							<p>SPENT</p>
+						</div>
+						<div class="benefit-box">
+							<h5>S$ <span>0.00</span></h5>
+							<p>BALANCE</p>
+						</div>
+						<div class="benefit-box">
+							<h5>S$ <span>0.00</span></h5>
+							<p>ALLOCATED</p>
+						</div>
+						<div class="benefit-box">
+							<h5>S$ <span>0.00</span></h5>
+							<p>TOTAL COMPANY CREDITS</p>
+						</div>
+					</div>
+				</div>
+
+    	</div>
+
+    	<div class="view-search-container">
+    		<div>
+    			<div class="view-toggle-btn">
+    				<h4>View</h4>
+    				<button>All <i class="fa fa-caret-down"></i></button>
+    			</div>
+    			<div class="search-wrapper">
+						<div class="input-group">
+							<input class="search-input" placeholder="Search Employee Name">
+							<ul v-if="false" class="dropdown-menu">
+								<li class="active">
+									<a class="dropdown-item" href="#" role="option">allan cheam alzula</a>
+								</li>
+								<li class="">
+									<a class="dropdown-item" href="#" role="option">allan cheam alzula</a>
+								</li>
+								<li class="">
+									<a class="dropdown-item" href="#" role="option">allan cheam alzula</a>
+								</li>
+							</ul>
+				      <span class="input-group-btn">
+				        <button><i class="fa fa-search"></i></button>
+				        <button v-if="false"><i class="fa fa-close"></i></button>
+				      </span>
+						</div>
+					</div>
+    		</div>
+    	</div>
+
+    	<div class="total-claim-trasaction">
+    		<div class="total-transaction-container"><span>124</span> Total Transactions</div>
+    		<div class="total-claim-container">Total Claim <span>S$ <span>811.05</span></span></div>
+    	</div>
+
+    	<div class="download-btn-container">
+    		<button>Download CSV <i class="fa fa-download"></i></button>
+    		<button>Download receipts <i class="fa fa-download"></i></button>
+    	</div>
+
+    </div>
+
+    <div class="transaction-wrapper">
+    	<div class="transaction-table-container">
+	    	<table>
+	    		<thead>
+	    			<tr>
+	    				<th></th>
+	    				<th>CLAIM DATE</th>
+	    				<th>APPROVED/ REJECTED DATE</th>
+	    				<th>CLAIM TYPE</th>
+	    				<th>PROVIDER</th>
+	    				<th>CLAIM AMOUNT</th>
+	    				<th>MEMBER</th>
+	    				<th></th>
+	    			</tr>
+	    		</thead>
+	    		<tbody>
+	    			<tr>
+	    				<td>
+	    					<label class="status-text pending">Pending</label>
+	    				</td>
+	    				<td>
+	    					<span>15 May 2019 05:09 PM</span>
+	    				</td>
+	    				<td>
+	    					<span>09 May 2019 04:21 PM</span>
+	    				</td>
+	    				<td class="claim-type-details">
+	    					<span>General Practice</span>
+	    				</td>
+	    				<td>
+	    					<span>qwer</span>
+	    				</td>
+	    				<td> S$
+	    					<span>12.00</span>
+	    				</td>
+	    				<td>
+	    					<span>Chryst Gundran</span>
+	    				</td>
+	    				<td>
+	    					<i class="fa fa-angle-down"></i>
+	    				</td>
+	    			</tr>
+	    			<tr class="in-network-subtr">
+	    				<td colspan="8">
+	    					<div class="status-box-left">
+	    						<div class="status_text">Pending</div>
+	    						<div class="claim-date-text">Claim Date: <span>15 May 2019 05:09 PM</span></div>
+	    					</div>
+	    					<div class="transac-details-wrapper">
+		    					<div class="transac-details">
+		    						<div>
+		    							<label>MEMBER</label>
+		    							<span>Chryst Gundran</span>
+		    						</div>
+		    						<div>
+		    							<label>APPROVED DATE</label>
+		    							<span></span>
+		    						</div>
+		    						<div>
+		    							<label>CLAIM DATE</label>
+		    							<span>15 May 2019 05:09 PM</span>
+		    						</div>
+		    						<div>
+		    							<label>VISIT DATE</label>
+		    							<span>15 May 2019 08:00 AM</span>
+		    						</div>
+		    						<div>
+		    							<label>TRANSACTION #</label>
+		    							<span>MNF000340</span>
+		    						</div>
+		    						<div>
+		    							<label>CLAIM TYPE</label>
+		    							<span>General Practice</span>
+		    						</div>
+		    						<div>
+		    							<label>PROVIDER</label>
+		    							<span>qwer</span>
+		    						</div>
+		    						<div>
+		    							<label>CLAIM AMOUNT</label>
+		    							<span>S$ 12.00</span>
+		    						</div>
+		    						<div>
+		    							<label>PAYMENT TYPE</label>
+		    							<span>E-Claim</span>
+		    						</div>
+		    						<div>
+		    							<label>REMARKS</label>
+		    							<span></span>
+		    						</div>
+		    					</div>
+	    					</div>
+	    					<div></div>
+	    				</td>
+	    			</tr>
+	    		</tbody>
+	    	</table>
+    	</div>
+    </div>
   </div>
 </template>
 
