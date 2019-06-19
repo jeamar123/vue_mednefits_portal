61b72f3763c3c1deeb68651d7718b00286ee9ed4
noelou jan <noelounagac@gmail.com>
2019-06-19 11:04:11 +0800

    add contents in the statement page

--- src/views/company/Statement.vue
+++ src/views/company/Statement.vue
@@ -1,6 +1,33 @@
 <template>
-  <div>
-    <h1>Statement</h1>
+  <div class="statement-wrapper">
+
+  	<div class="container">
+  		<div class="statement-container">
+  			<div>
+  				<span class="timeframe-title">Select a timeframe</span>
+  				<span class="year-selector"><a class="active">This year</a></span>
+  				<span class="year-selector"><a>Last year</a></span>
+  				<span class="year-selector"><a>Custom</a></span>
+  			</div>
+
+  			<div class="team-benefits-wrapper">
+					
+					<h4>View Statement</h4>
+					
+					<div class="view-statement-container">
+
+						<span>Choose how you want your statement to be shown</span>
+
+						<div class="btn-statement-wrapper">
+							<button @click="statementType(0, 'overview')" v-bind:class="{'active': viewStatementType.value === 0}" class="btn-spending-type btn-overview">Overview</button>
+							<button @click="statementType(1, 'full')" v-bind:class="{'active': viewStatementType.value === 1}" class="btn-spending-type btn-full">Full</button>
+						</div>
+					</div>	
+				</div>
+
+  		</div>
+  	</div>
+    
   </div>
 </template>
 
