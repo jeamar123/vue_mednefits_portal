8dda1bebe4e923a0549cab349bf983c212eaa337
noelou jan <noelounagac@gmail.com>
2019-06-19 11:04:11 +0800

    add contents in the statement page

--- src/components/company/statement.vue
+++ src/components/company/statement.vue
@@ -4,11 +4,24 @@
 let statement = {
   data() {
     return {
-      sampleData: true
+      viewStatementType: {
+    		value: 0,
+        text: ""
+      },
     };
   },
+  methods: {
+  	statementType(value, text) {
+      //spending either medical or wellness
+      this.viewStatementType.value = value;
+      this.viewStatementType.text = text;
+    },
+  }
 };
 
 export default statement
 </script>
 
+<style lang="scss" scoped>
+@import "./src/assets/css/company/statement.scss";
+</style>
