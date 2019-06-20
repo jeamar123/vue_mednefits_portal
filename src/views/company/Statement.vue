82f620108f307f39c9a2312673144e46505ae08e
noelou jan <noelounagac@gmail.com>
2019-06-20 17:24:17 +0800

    add contents in the statement of overview

--- src/views/company/Statement.vue
+++ src/views/company/Statement.vue
@@ -100,7 +100,112 @@
       </h4>
     </div>
     <div class="statement-overview-container">
-      
+      <div v-if="overview_active" class="statement-wrapper">
+        <div class="statement-header">
+          <div class="mednefits-img-container">
+            <img :src="'../assets/img/mednefits_logo_v3_(white).png'">
+          </div>
+          <div class="invoice-details-container">
+            <h2>INVOICE</h2>
+            <div class="medicloud-pte-text">Medicloud Pte Ltd</div>
+            <div>7 Temasek Boulevard</div>
+            <div>#18-02 Suntec Tower</div>
+            <div>038987</div>
+            <div class="country-text">Singapore</div>
+            <div>+65 6254 7889</div>
+            <div>mednefits.com</div>
+          </div>
+        </div>
+
+        <div v-if="false" class="no-transaction-month-container">
+          <div>No Transactions for this Month.</div>
+          <div>( <span>1 June</span> - <span>30 June </span> <span>2019</span> )</div>
+        </div>
+
+        <div class="transaction-month-container">
+
+          <div class="benefits-statement-container">
+            <div class="benefits-statement-col-1">
+              <div>Team Benefits Statement</div>
+              <div>Mednefits</div>
+              <div class="company-address">
+                7 Temasek Boulevard #18-02 Suntec T
+              </div>
+              <div>Fillbert Singapore</div>
+              <div>62547889</div>
+              <div>filbert@mednefits.com</div>
+            </div>
+            <div class="benefits-statement-col-2">
+              <div>
+                <label>Statement Number:</label>
+                <span>MC00000036</span>
+              </div>
+              <div>
+                <label>Statement Date:</label>
+                <span>1 Apr 2019</span>
+              </div>
+              <div>
+                <label>Payment Due:</label>
+                <span>16 Apr 2019</span>
+              </div>
+              <div>
+                <label>Amount Due (SGD):</label>
+                <span>$270.00</span>
+              </div>
+            </div>
+          </div>
+
+           <div class="summary-charges-container">
+            <h4>Summary of Charges</h4>
+
+            <div class="charges-row">
+              <div class="spending-account-container">
+                In-Network Spending Account Usage
+                <span class="pull-right-amount">S$ 153</span>
+              </div>
+              <div class="statement-month-container">
+                Statement for
+                <span>1 March</span> - 
+                <span>31 March</span>
+                <span> 2019</span>
+              </div>
+              <div class="charges-lite-plan">
+                Consultation Spent - General Practitioner 
+                <span class="pull-right-amount">S$ 117.00</span>
+              </div>
+            </div>
+
+            <div class="charges-row sub-total-container">
+              Sub Total
+              <span class="pull-right-amount">S$ 270.00</span>
+            </div>
+
+            <div class="total-due-container">
+              Total Due <span class="pull-right-amount">S$ 270.00</span>
+            </div>
+          </div>
+
+          <div class="payment-information-container">
+            <div class="payment-info-title">Payment Information</div>
+
+            <div class="cheque-payment-container">
+              <div class="cheque-bank-title">Cheque Payment:</div>
+              <div class="cheque-detail">Medicloud Pte Ltd</div>
+              <div class="cheque-detail">7 Temasek Boulevard #18-02 Suntec Tower</div>
+              <div class="cheque-detail">038987 Singapore</div>
+            </div>
+
+            <div class="bank-transfer-container">
+              <div class="cheque-bank-title">Bank Transfer:</div>
+              <div class="bank-detail">Bank: UOB</div>
+              <div class="bank-detail">Account Name: Medicloud Pte Ltd</div>
+              <div class="bank-detail">Account Number: 3743069399</div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
     </div>
     
   </div>
