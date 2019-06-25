d71f3c252df522b2f6b6c9f04ad2445e2365ed4c
noelou jan <noelounagac@gmail.com>
2019-06-25 16:16:52 +0800

    remove the transition in the button in statement

--- src/views/company/Statement.vue
+++ src/views/company/Statement.vue
@@ -52,79 +52,77 @@
 						</div>
 					</div>
           
-          <transition name="fade">
-            <div v-if="full_active" class="health-spending-container">
-              
-              <span class="header-content">Choose which Health Spending Account</span>
+          
+          <div v-if="full_active" class="health-spending-container">
+            
+            <span class="header-content">Choose which Health Spending Account</span>
 
-              <div class="btn-health-spending-wrapper">
-                <button @click="setSpendingType(0, 'medical')" v-bind:class="{'active': spendingTypeOpt.value === 0}" class="btn-spending-type btn-medical">Medical</button>
-                <button @click="setSpendingType(1, 'wellness')" v-bind:class="{'active': spendingTypeOpt.value === 1}" class="btn-spending-type btn-wellness">Wellness</button>
-                <button @click="setSpendingType(2, 'both')" v-bind:class="{'active': spendingTypeOpt.value === 2}" class="btn-spending-type btn-both">Both</button>
-              </div>
+            <div class="btn-health-spending-wrapper">
+              <button @click="setSpendingType(0, 'medical')" v-bind:class="{'active': spendingTypeOpt.value === 0}" class="btn-spending-type btn-medical">Medical</button>
+              <button @click="setSpendingType(1, 'wellness')" v-bind:class="{'active': spendingTypeOpt.value === 1}" class="btn-spending-type btn-wellness">Wellness</button>
+              <button @click="setSpendingType(2, 'both')" v-bind:class="{'active': spendingTypeOpt.value === 2}" class="btn-spending-type btn-both">Both</button>
             </div>
-          </transition>
+          </div>
+          
 				</div>
   		</div>
 
       <!-- overview -->
-      <transition name="fade">
-        <div v-if="overview_active" class="download-container">DOWNLOAD <img :src="'../assets/img/coverage/Download.png'"></div>
-      </transition>
+      <div v-if="overview_active" class="download-container">DOWNLOAD <img :src="'../assets/img/coverage/Download.png'"></div>
+      
 
 
       <!-- full -->
-      <transition name="fade">
-        <div v-if="full_active" class="statement-header-wrapper">
-          <div class="search-wrapper">
-            <div class="input-group">
-              <input class="search-input" placeholder="Search Employee Name">
-              <ul v-if="false" class="dropdown-menu">
-                <li class="active">
-                  <a class="dropdown-item" href="#" role="option">allan cheam alzula</a>
-                </li>
-                <li class="">
-                  <a class="dropdown-item" href="#" role="option">allan cheam alzula</a>
-                </li>
-                <li class="">
-                  <a class="dropdown-item" href="#" role="option">allan cheam alzula</a>
-                </li>
-              </ul>
-              <span class="input-group-btn">
-                <button><i class="fa fa-search"></i></button>
-                <button v-if="false"><i class="fa fa-close"></i></button>
-              </span>
+      <div v-if="full_active" class="statement-header-wrapper">
+        <div class="search-wrapper">
+          <div class="input-group">
+            <input class="search-input" placeholder="Search Employee Name">
+            <ul v-if="false" class="dropdown-menu">
+              <li class="active">
+                <a class="dropdown-item" href="#" role="option">allan cheam alzula</a>
+              </li>
+              <li class="">
+                <a class="dropdown-item" href="#" role="option">allan cheam alzula</a>
+              </li>
+              <li class="">
+                <a class="dropdown-item" href="#" role="option">allan cheam alzula</a>
+              </li>
+            </ul>
+            <span class="input-group-btn">
+              <button><i class="fa fa-search"></i></button>
+              <button v-if="false"><i class="fa fa-close"></i></button>
+            </span>
+          </div>
+        </div>
+        <div class="statement-transaction-header">
+          <div>
+            <div class="total-transactions-container">
+              <span> </span> Total Transactions
+            </div>
+            <div class="total-spent-container"> Total Spent
+              <span>S$ <span>0.00</span> </span> 
             </div>
           </div>
-          <div class="statement-transaction-header">
-            <div>
-              <div class="total-transactions-container">
-                <span> </span> Total Transactions
-              </div>
-              <div class="total-spent-container"> Total Spent
-                <span>S$ <span>0.00</span> </span> 
-              </div>
-            </div>
-            <div>
-              <div class="btn-transaction-container">
-                <button @click="netType(0, 'In-network')" v-bind:class="{'active': networkType.value === 0}">
-                  IN-NETWORK <br> TRANSACTIONS
-                </button>
-                <button @click="netType(1, 'In-network')" v-bind:class="{'active': networkType.value === 1}">
-                  E-CLAIM TRANSACTIONS <br> (OUT-OF-NETWORK)
-                </button>
-              </div>
-            </div>
-            <div>
-              <div class="download-statement-container">
-                <span>Download Statement</span>
-                <button class="btn-pdf">PDF</button>
-                <button class="btn-csv">CSV</button>
-              </div>
+          <div>
+            <div class="btn-transaction-container">
+              <button @click="netType(0, 'In-network')" v-bind:class="{'active': networkType.value === 0}">
+                IN-NETWORK <br> TRANSACTIONS
+              </button>
+              <button @click="netType(1, 'In-network')" v-bind:class="{'active': networkType.value === 1}">
+                E-CLAIM TRANSACTIONS <br> (OUT-OF-NETWORK)
+              </button>
             </div>
           </div>
-        </div> 
-      </transition>
+          <div>
+            <div class="download-statement-container">
+              <span>Download Statement</span>
+              <button class="btn-pdf">PDF</button>
+              <button class="btn-csv">CSV</button>
+            </div>
+          </div>
+        </div>
+      </div> 
+      
 
   	</div>
 
