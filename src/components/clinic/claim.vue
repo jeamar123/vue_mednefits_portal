f2a8877aaee5767ed86f74cfd19d3e6900235bd6
noelou jan <noelounagac@gmail.com>
2019-07-26 19:59:19 +0800

    added ui for claim

---
+++ src/components/clinic/claim.vue
@@ -0,0 +1,25 @@
+<script>
+	import axios from 'axios'
+
+	var claim = {
+		data() {
+			return {
+				showIconLoader : false,
+			}
+		},
+		
+	
+	}
+
+	export default claim
+</script>
+
+<style lang="scss" scoped>
+@import "./src/assets/css/clinic/claim.scss";
+</style>
+
+
+
+
+
+
