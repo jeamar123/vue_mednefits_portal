2fe8aaa8cbdc2f2a232b4d9cecefea53b8e29452
Jazer Kemuel Zayas <jaz@Jazers-MacBook-Air.local>
2019-09-18 14:50:22 +0800

    integrate calendar - Clinic UI

---
+++ src/components/clinic/calendar.vue
@@ -0,0 +1,39 @@
+<script>
+import Modal from "../../views/clinic/modal/Modal.vue";
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from "@fullcalendar/timegrid";
+
+var calendar = {
+	components: {
+		Modal,
+		FullCalendar
+	},
+	data() {
+		return {
+			calendarPlugins: [
+				dayGridPlugin,
+				timeGridPlugin,
+			],
+			calendarView: 'dayGridMonth', // dayGridMonth,timeGridWeek,timeGridDay
+			dataSample: false
+		};
+	}
+};
+
+export default calendar;
+</script>
+
+<style lang="scss" scoped>
+@import "./src/assets/css/clinic/calendar.scss";
+@import "./src/assets/css/clinic/clinic-responsive.scss";
+@import '~@fullcalendar/core/main.css';
+@import '~@fullcalendar/daygrid/main.css';
+@import '~@fullcalendar/timegrid/main.css';
+</style>
+
+
+
+
+
+
---
+++ src/views/clinic/Calendar.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="calendar-container">
+		<div class="clinic-calendar-wrapper">
+			Calendar ni sya besh
+	
+			<FullCalendar :defaultView="calendarView"
+				ref="fullCalendar" 
+				:plugins="calendarPlugins" 
+				:weekends="false" 
+				:header = false
+				:events="[
+					{ title: 'event 1', date: '2019-09-09' },
+					{ title: 'event 2', date: '2019-09-09' }
+				]"
+			/>
+			<!-- 
+				:header="{
+					right: 'today prev,next',
+					center: 'title',
+					left: 'dayGridMonth,timeGridWeek,timeGridDay'
+      	}"
+			 -->
+		</div>
+	
+	</div>
+</template>
+
+<script>
+import calendar from "@/components/clinic/calendar";
+export default calendar;
+</script>
